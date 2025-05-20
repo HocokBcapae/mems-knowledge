e73ccb8870184e14935668ec96716ba15d525d81
HocokBcapae <vladimir.klimenkin@gmail.com>
2025-05-20 17:59:04 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'https://hocokbcapae.github.io/publiv-views/', // <-- use your repo name here
+  base: '/mems-knowledge/', // <-- repo name here
   plugins: [vue()],
 })
